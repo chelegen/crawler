fb6ff97507982f9b0718609d3a22bab560ef49c3
emmmm <3108775859@qq.com>
2020-01-02 00:53:10 +0800

    removed unused imports

--- src/main/java/com/github/emmm/Main.java
+++ src/main/java/com/github/emmm/Main.java
@@ -1,8 +1,5 @@
 package com.github.emmm;
 
-import java.io.IOException;
-import java.sql.SQLException;
-
 public class Main {
     public static void main(String[] args) {
         CrawlerDao dao = new MyBatisCrawlerDao();
